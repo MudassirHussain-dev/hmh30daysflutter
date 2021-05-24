f039b63ea53cff857c3d5ffb3b5484e71618edfd
Mudassir Hussain <Mudassir Hussain@DESKTOP-68T6GGT>
2021-05-24 21:08:15 +0500

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          home: Material(
-        child: Center(
-          child: Container(
-          child:Text("MashAllah")
-      ),
-        ),
-          ),
+      home: Home(),
     );
   }
-}
