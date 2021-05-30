836aef9d635c764f19cef971feeb3c15f278ff54
Mudassir Hussain <Mudassir Hussain@DESKTOP-68T6GGT>
2021-05-30 21:30:42 +0500

    day6completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,10 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        centerTitle: true,
+      ),
       body: Center(
         child: Container(
           child: Text("MashAllah")),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/routes.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-                  child: Column(
+          child: Column(
             children: [
               Image.asset(
                 "assets/images/login_image.png",
@@ -17,20 +24,24 @@ class Login extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
               ),
               SizedBox(
                 height: 40.0,
               ),
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 18.0),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 18.0),
                 child: Column(
                   children: [
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter UserName", labelText: "UserName"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -40,13 +51,41 @@ class Login extends StatelessWidget {
                     SizedBox(
                       height: 20.0,
                     ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(minimumSize: Size(300,40)),
-                      onPressed: () {
-                       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    InkWell(
+                      onTap: ()async{
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: Text("Login"),
-                    )
+                      child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          //wrap with GestureDetector and inkwell
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child:changeButton? Icon(Icons.done,color:Colors.white) : Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.deepPurple,
+                              //  shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(
+                                  changeButton ? 50 : 8.0))),
+                    ),
+
+                    // ElevatedButton( 
+                    //   style: TextButton.styleFrom(minimumSize: Size(300, 40)),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Text("Login"),
+                    // )
                   ],
                 ),
               )
